68b64501f60029c19d46daf0fc744030d89cc58f
PujaTiwa <pujat4582gamil.com>
2024-03-09 21:17:26 +0530

    Added display question section

--- src/AllRoutes.jsx
+++ src/AllRoutes.jsx
@@ -2,12 +2,18 @@ import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Auth from './pages/Auth/Auth.jsx';
 import Home from './pages/Home/Home.jsx';
+import Questions from './pages/Questions/Questions.jsx';
+import AskQuestion from './pages/AskQuestion/AskQuestion.jsx';
+import DisplayQuestion from './pages/Questions/DisplayQuestion.jsx';
 
 const AllRoutes = () => {
   return (
     <Routes>
       <Route path='/' element={<Home />} />
       <Route path='/Auth' element={<Auth />} />
+      <Route path='/Questions' element={<Questions />} />
+      <Route path='/AskQuestion' element={<AskQuestion />} />
+      <Route path='/Questions/:id' element={<DisplayQuestion />} />
     </Routes>
   )
 }
---
+++ src/pages/Questions/DisplayQuestion.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import LeftSidebar from '../../components/LeftSidebar/LeftSidebar.jsx'
+import RightSidebar from '../../components/RightSidebar/RightSidebar.jsx'
+import QuestionsDetails from './QuestionsDetails.jsx'
+
+const DisplayQuestion = () => {
+   
+  return (
+    <div className='home-container-1'>
+    <LeftSidebar />
+    <div className='home-container-2'>
+      <QuestionsDetails />
+      <RightSidebar />
+    </div>
+  </div>
+  )
+}
+
+export default DisplayQuestion
